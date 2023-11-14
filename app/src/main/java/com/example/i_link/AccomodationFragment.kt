0c38612ea600b26866044673cf43ee108f110992
jeb <jblas0151@gmail.com>
2023-11-14 16:05:48 -0600

    Accomodation fragment

---
+++ app/src/main/java/com/example/i_link/AccomodationFragment.kt
@@ -0,0 +1,33 @@
+package com.example.i_link
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.i_link.databinding.FragmentAccomodationBinding
+import com.example.i_link.databinding.FragmentFirstBinding
+
+class AccomodationFragment: Fragment() {
+    private var _binding: FragmentAccomodationBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentAccomodationBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
